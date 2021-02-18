4f93b4f0cc9b512e0f1a045dec73dfceff5f4bdb
Rembrandtalvaradoc <teslafloyd@gmail.com>
2021-02-18 18:28:24 -0500

    Clase es par
    ola

---
+++ Supbro.py
@@ -0,0 +1,9 @@
+class esPar:
+    def __init__(self):
+        pass
+    
+    def esFarc(self, num1):
+        if(num1 % 2 == 0):
+            return "siks, es par"
+        else:
+            return "noks socio, cambie eso"
